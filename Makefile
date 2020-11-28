68d57f82819db2cfc9d836598a91df64088755bb
Jeremy Weinstein <jeremydw@gmail.com>
2020-11-27 19:51:00 -0800

    Update tests

--- Makefile
+++ Makefile
@@ -2,5 +2,12 @@
 test:
 	pipenv run python -m grow_static_server.main_test
 
+curl-test:
+	GROW_STATIC_SERVER_POD_ROOT=test \
+	  pipenv run gunicorn -b :9999 -w 2 grow_static_server.main:app &
+	sleep 4s
+	curl http://localhost:9999/
+	pkill -f gunicorn
+
 install:
 	pipenv install -d
--- grow_static_server/main_test.py
+++ grow_static_server/main_test.py
@@ -14,10 +14,30 @@ class TestEndpoints(unittest.TestCase):
     def setUpClass(cls):
         cls.app = webtest.TestApp(app)
 
-    def test_root(self):
+    def test(self):
+        # Test root.
         resp = self.app.get('/')
         self.assertEqual(200, resp.status_int)
         self.assertIn('Hello World', resp.body.decode())
+
+        # Test 404.
+        resp = self.app.get('/does-not-exist/', status=404)
+        self.assertEqual(404, resp.status_int)
+
+        # Test trailing slash redirect.
+        resp = self.app.get('/test')
+        self.assertEqual(302, resp.status_int)
+        self.assertEqual('https://example.com/', resp.headers['Location'])
+
+        # Test vanity URLs.
+        resp = self.app.get('/test/foo')
+        self.assertEqual(302, resp.status_int)
+        self.assertEqual('https://example.com/foo', resp.headers['Location'])
+
+        # Test trailing slash redirect.
+        resp = self.app.get('/subfolder')
+        self.assertEqual(302, resp.status_int)
+        self.assertEqual('http://localhost/subfolder/', resp.headers['Location'])
 
 
 if __name__ == '__main__':
